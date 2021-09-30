23bc913c11823ba792d5bab69134dbd9dce6f160
tvannost <tvannost@hotmail.com>
2021-09-30 11:57:14 -0400

    added axios

--- Repo/main.js/.js
+++ Repo/main.js/.js
@@ -1,3 +1,18 @@
 //API key
 const key = "ae408ca0fc0a06a1700b29d9d60fd061";
 
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
