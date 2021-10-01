4a76d67a9c63fd6211c3cf5f4385fb09ce2fe4d6
tvannost <tvannost@hotmail.com>
2021-09-30 23:35:25 -0400

    more changes

--- Repo/main.js/.js
+++ Repo/main.js/.js
@@ -1,29 +1,36 @@
 // variables for data
-const inputZip = document.getElementById("zipSearch");
-const button = document.getElementById("button");
-const city = document.getElementById('city');
-const tempK = document.getElementById('tempK');
-const tempF = document.getElementById('tempF');
-const tempC = document.getElementById('tempC');
 const description = document.getElementById('description')
 const icons = document.getElementById('icons');
+
+var cityValue;
+var tempValue;
+var tempFValue;
+var tempCValue;
+var conditionValue;
+var zipValue;
 
 button.addEventListener('click', getWeather);
 
 function getWeather()   {
-    let zip = document.getElementById("zipSearch").value;
+    zipValue = document.getElementById("zipSearch").value;
+axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipValue},&appid=ae408ca0fc0a06a1700b29d9d60fd061`)
+ 
+.then(function (response) {
+    return response
+        cityValue = response.data.name;
+        tempValue = Math.round(response.data.main.temp);
+        tempFValue = Math.round((tempValue-273.15)*(9/5)+32);
+        tempCValue = Math.round((tempValue-273.15));
+        conditionValue = response.data.weather[0].description;
 
-axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=40515,us&appid=ae408ca0fc0a06a1700b29d9d60fd061`)
-  .then(function (response) {
-      city.innerHTML = data.name;
-            tempK.innerHTML = data.main.temp + '°' +' K'
-            tempF.innerHTML = Math.round((data.main.temp - 273.15) * 9 / 5 + 32) + '°' + ' F'
-            tempC.innerHTML = Math.round(json.main.temp - 273.15) + '°' + ' C'
-            description.innerHTML = data.weather[0].description
-            icons.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
+        city.innerHTML = cityValue;
+        tempK.innerHTML = tempValue + '°' +' K'
+        tempF.innerHTML = tempFValue + '°' + ' F'
+        tempC.innerHTML = tempCValue + '°' + ' C'
+        condition.innerHTML = conditionValue;
+            
   })
   .catch(function (error) {
-       document.getElementById("message").innerText = error
     city.innerHTML= "Please enter valid Zip Code"
   })
 }
